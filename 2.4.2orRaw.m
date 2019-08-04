6701d30adf0d6b9de4e107d39e38c72de0962835
TanjinaRahman <31213441+TanjinaRahman@users.noreply.github.com>
2019-08-04 08:39:45 +0600

    OR operation (Raw)

---
+++ 2.4.2orRaw.m
@@ -0,0 +1,11 @@
+A=imread('cir2.png');
+B=imread('cir3.png');
+A=im2bw(A);
+B=im2bw(B);
+C = A | B;
+subplot(2,2,1),imshow(A);
+title('A');
+subplot(2,2,2),imshow(B);
+title('B');
+subplot(2,2,3),imshow(C);
+title('A OR B');
