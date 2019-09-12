27c8032f777af53d8705c79efaeb33b6b66796bc
Tanjina Rahman <31213441+tanjina-3ni@users.noreply.github.com>
2019-09-12 09:26:38 +0600

    Min Filter

---
+++ 4Lminfilt.m
@@ -0,0 +1,25 @@
+im = imread('cameraman.tif');
+subplot(1,2,1),imshow(im);
+title('Original Image');
+
+m=3; n=3;
+f = padarray(im,[floor(n/2) floor(m/2)],'both');
+
+[r,c] = size(im);
+g = zeros(size(f));
+C = zeros(m,n);
+
+for i=2:r
+    for j=2:c
+        for k=1:3
+            for l=1:3
+                C(k,l) = f(i+k-2, j+l-2);
+            end
+        end
+        g(i,j) = round(min(C(:)));
+    end
+end
+
+subplot(1,2,2),imshow(uint8(g));
+title('Min Filtered Image');
+ 
